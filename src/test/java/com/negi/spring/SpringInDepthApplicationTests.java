ca5d3105c58f6adff95fb8c4ef309811d904ada2
shi514652697 <ssnegi_4all@yahoo.com>
2020-09-03 14:31:56 -0500

    fist commit

---
+++ src/test/java/com/negi/spring/SpringInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
